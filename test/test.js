3261a29c26da6826e9a7a09f1b13c51f51bd445f
Stephanie <40613732+steph-bot@users.noreply.github.com>
2019-11-12 22:15:57 -0600

    sample unit test and gitignore files

---
+++ test/test.js
@@ -0,0 +1,12 @@
+const assert = require ('assert');
+// could also use should.js
+
+const testData = {};
+const expectedInput = {};
+
+describe('Sample unit test', () => {
+	it('should run without errors.', () => {
+		const expectedOutput = {};
+		assert.deepStrictEqual(expectedInput, expectedOutput);
+	});
+});
